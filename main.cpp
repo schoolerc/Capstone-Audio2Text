5a7ce5fc49d23935f178f37f4eddac795aa64ec2
Chaz Schooler <chaz@cse498-amazon.cse.msu.edu>
2016-02-05 14:02:19 -0500

    Initial project setup

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
+#include <boost/log/trivial.hpp>
+
+int main(int argc, const char* argv[])
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
