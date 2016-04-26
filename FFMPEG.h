405e8411eccc9873e82ba4ebd6659f0df28408ee
Charles Schooler <school19@msu.edu>
2016-04-26 21:14:08 +0000

    Accidentally removed a line that was still needed

--- FFMPEG.h
+++
@@ -1,8 +0,0 @@
-extern "C"
-{
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavutil/avutil.h>
-#include <libavresample/avresample.h>
-#include <libavutil/opt.h>
-}
